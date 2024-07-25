1274eb1c57ec182938cd6a39dc5956c506e98c25
Oleh Halchenko <oleh.halchenko@appsdelivered.com>
2024-07-25 20:43:02 +0200

    hotfix

--- src/assets/icons/Icons.tsx
+++ src/assets/icons/Icons.tsx
@@ -3,6 +3,25 @@ import { SVGProps } from "react";
 export const SidebarLogo = () => {
   return (
     <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="#7E7E7E"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M11 19a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm10 2-4.35-4.35"
+      />
+    </svg>
+  );
+};
+export const SearchLogo = () => {
+  return (
+    <svg
       width="37"
       height="37"
       viewBox="0 0 37 37"
--- src/pages/CustomersPage/CustomersPage.tsx
+++ src/pages/CustomersPage/CustomersPage.tsx
@@ -1,7 +1,24 @@
+import "./CustomersPage.scss";
+import search from "@/assets/icons/search.svg";
+
 export const CustomersPage = () => {
   return (
-    <div className="in_development">
-      <h1>Customers page in development</h1>
+    <div className="customers-page">
+      <div className="customers-header">
+        <div className="customers-description">
+          <h1>All Customers</h1>
+          <p>Active Members</p>
+        </div>
+        <div className="customers-search">
+          {/* <input type="text" name="Search" id="" placeholder="Search" /> */}
+
+          <div className="input">
+            <input placeholder="Search" type="text" />
+            <img className="icon" src={search}></img>
+          </div>
+        </div>
+      </div>
+      <div className="customers-table"></div>
     </div>
   );
 };
