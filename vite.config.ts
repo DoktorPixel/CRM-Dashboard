96dd08df79b1d96b90e068746e08446ee3984a94
Oleh Halchenko <oleh.halchenko@appsdelivered.com>
2024-07-26 10:28:40 +0200

    base: "/CRM-Dashboard/"

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,12 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+// const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
 export default defineConfig({
   plugins: [react()],
+  // base: "/",
+  base: "/CRM-Dashboard/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
