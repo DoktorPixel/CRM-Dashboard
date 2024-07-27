144f9b7dfa92adaec2627f9ca3aeebc368a3acc9
Oleh Halchenko <oleh.halchenko@appsdelivered.com>
2024-07-27 16:18:22 +0200

    config

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,24 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// const __dirname = path.dirname(new URL(import.meta.url).pathname);
+// export default defineConfig({
+//   plugins: [react()],
+//   // base: "/",
+//   base: "CRM-Dashboard",
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
 
 export default defineConfig({
   plugins: [react()],
-  // base: "/",
-  base: "/CRM-Dashboard/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: "/CRM-Dashboard/",
 });
 
